b3de51648da5d123a355a0f203666aa570238d02
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-09-22 23:31:28 +0530

    Minor fixes

--- src/APICalls/index.js
+++ src/APICalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'youseai-backend-production.up.railway.app',
+    baseURL: 'http://localhost:8081',
     headers: {
         'Content-Type' : 'application/json',
         'authorization': `Bearer ${localStorage.getItem('token')}`
